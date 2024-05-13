4f1fd483f0a19132aae91dc829f27fae83e8ffd1
biubiu0628 <pewpew0628@gmail.com>
2024-05-13 16:07:26 +0700

    huutien

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import TaskItem from "./components/TaskItem";
 
 export default function App() {
+  //1
   const [todos, setTodos] = useState([]);
-
+  //2
   const [todo, setTodo] = useState({
     name: "",
     des: "",
   });
+  //3
+  const [isEditing, setIsEditing] = useState(false);
+  //4
+  const editedItemIndex = useRef(null);
+  // eslint-disable-next-line
+  const [isSaving, setIsSaving] = useState([]);
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((_, index) => index !== id));
+    setTodo({ name: "", des: "" });
+    setIsEditing(false);
+  };
+
+  const handleEdit = (index) => {
+    setIsEditing(true);
+    editedItemIndex.current = index;
+    setTodo({
+      name: todos[index].name,
+      des: todos[index].des,
+    });
+  };
+
+  const handleSaveEdit = () => {
+    const updatedTodos = [...todos];
+    updatedTodos[editedItemIndex.current] = todo;
+    setTodos(updatedTodos);
+    setIsEditing(false);
+    setTodo({ name: "", des: "" });
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setTodo({ name: "", des: "" });
+  };
+
   return (
-    <div className="h-screen px-96">
+    <div className="h-screen xl:px-96 md:px-36">
       <h1 className="text-4xl font-bold text-center p-4">Todo List</h1>
 
-      <div className="grid grid-cols-4 h-[80%] text-center bg-orange-300 p-4 gap-2 rounded mb-2">
-        {todos.map((todo) => (
-          <TaskItem name={todo.name} des={todo.des} />
+      <div className="grid grid-cols-4 max-xl:grid-cols-1 min-h-[80%] bg-orange-300 p-4 gap-2 rounded mb-2">
+        {todos.map((todo, index) => (
+          <TaskItem
+            name={todo.name}
+            des={todo.des}
+            onDelete={() => handleDelete(index)}
+            onEdit={() => handleEdit(index)}
+          />
         ))}
       </div>
 
-      <div className="flex bg-green-300 p-4 h-auto flex-auto gap-4 rounded">
+      <div className="grid grid-cols-[20%_20%_20%_20%_auto] max-xl:grid-cols-1 bg-green-300 p-4 h-auto gap-4 rounded">
         <input
           type="text"
           className="px-2 rounded"
@@ -32,7 +73,7 @@ export default function App() {
               name: e.target.value,
             })
           }
-        ></input>
+        />
         <input
           type="text"
           className="px-2 rounded"
@@ -45,15 +86,33 @@ export default function App() {
               des: e.target.value,
             })
           }
-        ></input>
-        <button
-          className=" bg-gray-400 rounded p-2 text-sm font-bold hover:bg-gray-300"
-          onClick={() => {
-            setTodos([...todos, todo]);
-          }}
-        >
-          Add Task
-        </button>
+        />
+        {isEditing ? (
+          <>
+            <button
+              className="bg-blue-400 rounded p-2 text-sm font-bold hover:bg-blue-300"
+              onClick={handleSaveEdit}
+            >
+              Save Edit
+            </button>
+            <button
+              className="bg-red-400 rounded p-2 text-sm font-bold hover:bg-red-300"
+              onClick={handleCancelEdit}
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <button
+            className="bg-gray-400 rounded p-2 text-sm font-bold hover:bg-gray-300"
+            onClick={() => {
+              setTodos([...todos, todo]);
+              setTodo({ name: "", des: "" });
+            }}
+          >
+            Add Task
+          </button>
+        )}
       </div>
     </div>
   );
